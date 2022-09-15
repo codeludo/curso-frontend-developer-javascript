3c4cf2cafd926bd38e132721bf99d50895289a1c
Juan Camilo Acevedo Moreno <jacevedo@todo1.net>
2022-09-14 22:24:43 -0500

    mobile menu & hamburger menu

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+
+const hamMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
+hamMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     let classes = desktopMenu.classList
+    const inactive = classes.toggle('inactive') 
+}
+function toggleMobileMenu(){
+    let classes = mobileMenu.classList
     const inactive = classes.toggle('inactive') 
 }
