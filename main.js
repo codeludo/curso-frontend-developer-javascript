36b9bcb72da124b5aa25b79a6e7d2fc4664350e7
Juan Camilo Acevedo Moreno <jacevedo@todo1.net>
2022-09-14 22:59:19 -0500

    aside with shopping cart

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
+// html elements (js objects)
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
 const hamMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
+// listeners
 menuEmail.addEventListener('click', toggleDesktopMenu)
 hamMenu.addEventListener('click', toggleMobileMenu)
+menuCarrito.addEventListener('click', toggleCarritoAside)
 
+
+
+// functions
+
+// menu desktop
 function toggleDesktopMenu(){
-    let classes = desktopMenu.classList
-    const inactive = classes.toggle('inactive') 
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
+    desktopMenu.classList.toggle('inactive') 
 }
+
+// menu mobile
 function toggleMobileMenu(){
-    let classes = mobileMenu.classList
-    const inactive = classes.toggle('inactive') 
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+// aside
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
